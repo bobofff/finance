1b15cd6dd0b6d2a9241731ea7b2ebf3e8b963cc6
bobo <bobo@bobodeMac-mini.local>
2026-02-12 18:37:01 +0800

    222

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,9 +7,6 @@ cd "$ROOT_DIR"
 echo "==> Pull latest code"
 git pull
 
-echo "==> Reset deploy/.env from deploy/.env.prod"
-cp deploy/.env.prod deploy/.env
-
 echo "==> Build and restart containers"
 docker compose -f deploy/docker-compose.yml --env-file deploy/.env up -d --build
 
